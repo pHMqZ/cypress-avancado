3fb95c04265a6ec513bcb8327665fae14829f734
Phillip Marques <phillipmarq@hotmail.com>
2024-08-27 17:22:55 -0300

    test_feat: Creating tests cases for simulate errors

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -69,14 +69,6 @@ describe('Hacker Stories', () => {
       it('orders by points', () => {})
     })
 
-    // Hrm, how would I simulate such errors?
-    // Since I still don't know, the tests are being skipped.
-    // TODO: Find a way to test them out.
-    context.skip('Errors', () => {
-      it('shows "Something went wrong ..." in case of a server error', () => {})
-
-      it('shows "Something went wrong ..." in case of a network error', () => {})
-    })
   })
 
   context('Search', () => {
@@ -123,6 +115,19 @@ describe('Hacker Stories', () => {
         .should('be.visible')
     })
 
+    /*
+    Alternative to test cases, but doesn't simulate use as a user
+    it('types and submits the form directly', () =>{
+      cy.get('#search')
+        .type(newTerm)
+      cy.get('form').submit()
+
+      cy.wait('@getNewTermStories')
+
+      cy.get('.item').should('have.length', 20)
+    })
+    */
+
     context('Last searches', () => {
       it('searches via the last searched term', () => {
         cy.get('#search')
@@ -163,5 +168,38 @@ describe('Hacker Stories', () => {
           .should('have.length', 5)
       })
     })
+  })
+})
+
+context.only('Errors', () => {
+  beforeEach(() => {
+    
+  })
+  it('shows "Something went wrong ..." in case of a server error', () => {
+    cy.intercept(
+      'GET',
+      '**/search**',
+      { statusCode: 500 }
+    ).as('getServerFailure')    
+
+    cy.visit('/')
+    cy.wait('@getServerFailure')
+
+    cy.get('p:contains(Something went wrong ...)')
+      .should('be.visible')
+  })
+
+  it('shows "Something went wrong ..." in case of a network error', () => {
+    cy.intercept(
+      'GET',
+      '**/search**',
+      { forceNetworkError: true }
+    ).as('getNetworkFailure') 
+
+    cy.visit('/')
+    cy.wait('@getNetworkFailure')
+
+    cy.get('p:contains(Something went wrong ...)')
+      .should('be.visible')
   })
 })
