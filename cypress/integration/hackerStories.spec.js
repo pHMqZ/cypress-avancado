96030793370fdfbffe10e3e1440296d63f1de817
Phillip Marques <phillipmarq@hotmail.com>
2024-09-14 16:34:57 -0300

    teste_feat: Simulating API call delays

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -350,3 +350,22 @@ context('Errors', () => {
       .should('be.visible')
   })
 })
+
+it.only('shows a "Loading..." state before showing the results', () =>{
+  cy.intercept(
+    'GET',
+    '**/search**',
+    { 
+      delay : 2000,
+      fixture: 'stories'
+    }
+  ).as('getDelayedStories')
+
+  cy.visit('/')
+  
+  cy.assertLoadingIsShownAndHidden()
+  cy.wait('@getDelayedStories')
+  
+  cy.get('.item').should('have.length', 2)
+
+})
