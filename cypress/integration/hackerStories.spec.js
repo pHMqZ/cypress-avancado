78eb48b2a7fcbede4c8c49330cf670a746a593b4
Phillip Marques <phillipmarq@hotmail.com>
2024-09-14 15:50:47 -0300

    test_refactor: making tests more efficient_2

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -29,7 +29,9 @@ describe('Hacker Stories', () => {
 
       cy.get('.item').should('have.length', 20)
 
-      cy.contains('More').click()
+      cy.contains('More')
+        .should('be.visible')
+        .click()
 
       cy.wait('@getNextStories')
 
@@ -43,6 +45,7 @@ describe('Hacker Stories', () => {
       ).as('getNewTermStories')
 
       cy.get('#search')
+        .should('be.visible')
         .clear()
         .type(`${newTerm}{enter}`)
 
@@ -57,7 +60,8 @@ describe('Hacker Stories', () => {
       cy.get('.item').should('have.length', 20)
       cy.get('.item')
         .first()
-        .should('contain', initialTerm)
+        .should('be.visible')
+        .and('contain', initialTerm)
       cy.get(`button:contains(${newTerm})`)
         .should('be.visible')
     })
@@ -88,6 +92,7 @@ describe('Hacker Stories', () => {
         it('shows the right data for all rendered stories', () => {
           cy.get('.item')
             .first()
+            .should('be.visible')
             .should('contain', stories.hits[0].title)
             .and('contain', stories.hits[0].author)
             .and('contain', stories.hits[0].num_comments)
@@ -97,6 +102,7 @@ describe('Hacker Stories', () => {
 
           cy.get('.item')
             .last()
+            .should('be.visible')
             .should('contain', stories.hits[1].title)
             .and('contain', stories.hits[1].author)
             .and('contain', stories.hits[1].num_comments)
@@ -108,6 +114,7 @@ describe('Hacker Stories', () => {
         it('shows one less story after dimissing the first one', () => {
           cy.get('.button-small')
             .first()
+            .should('be.visible')
             .click()
 
           cy.get('.item').should('have.length', 1)
@@ -118,6 +125,7 @@ describe('Hacker Stories', () => {
           it('orders by title', () => {
             cy.get('.list-header-button:contains(Title)')
               .as('titleHeader')
+              .should('be.visible')
               .click()
 
             cy.get('.item')
@@ -141,6 +149,7 @@ describe('Hacker Stories', () => {
           it('orders by author', () => {
             cy.get('.list-header-button:contains(Author)')
               .as('authorHeader')
+              .should('be.visible')
               .click()
 
             cy.get('.item')
@@ -160,6 +169,7 @@ describe('Hacker Stories', () => {
           it('orders by comments', () => {
             cy.get('.list-header-button:contains(Comments)')
               .as('commentsHeader')
+              .should('be.visible')
               .click()
 
             cy.get('.item')
@@ -179,6 +189,7 @@ describe('Hacker Stories', () => {
           it('orders by points', () => {
             cy.get('.list-header-button:contains(Points)')
               .as('pointsHeader')
+              .should('be.visible')
               .click()
 
             cy.get('.item')
@@ -216,6 +227,7 @@ describe('Hacker Stories', () => {
         cy.wait('@getEmptyStories')
 
         cy.get('#search')
+          .should('be.visible')
           .clear()
       })
 
@@ -225,6 +237,7 @@ describe('Hacker Stories', () => {
 
       it('types and hits ENTER', () => {
         cy.get('#search')
+          .should('be.visible')
           .type(`${newTerm}{enter}`)
 
         cy.wait('@getStories')
@@ -236,8 +249,10 @@ describe('Hacker Stories', () => {
 
       it('types and clicks the submit button', () => {
         cy.get('#search')
+          .should('be.visible')
           .type(newTerm)
         cy.contains('Submit')
+          .should('be.visible')
           .click()
 
         cy.wait('@getStories')
